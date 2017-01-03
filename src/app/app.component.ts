8043c4a74e401a210909b17d0fec2ac6ebcec888
Nikhil Arora <narora200@gmail.com>
2017-01-04 01:24:58 +0530

    Done till work of nikhil on 3.1.17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,6 +44,7 @@ export class MyApp {
     // close the menu when clicking a link from the menu
     this.menu.close();
     // navigate to the new page if it is not the current page
-    this.nav.setRoot(page.component);
+    //this.nav.setRoot(page.component);
+      this.nav.push(page.component);
   }
 }
