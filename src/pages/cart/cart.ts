c9f4f6d2ce471f9670657910d14a05c4b178be2c
Nikhil Arora <narora200@gmail.com>
2017-01-09 20:29:16 +0530

    Cart Page functioning all well

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -14,8 +14,10 @@ import { Storage } from '@ionic/storage';
 export class CartPage {
 public cartItems: Array<any> = [];
 public cartArray: Array<any> = [];
+public costSum = 0;
+public costSumString;
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
-
+var that = this;
     this.storage.get('cartProducts').then((val)=> {
         
        console.log('On the Cart Page: ', val);
@@ -24,7 +26,11 @@ public cartArray: Array<any> = [];
 
 this.cartItems = this.cartArray;
 console.log(this.cartItems);
-
+this.cartItems.forEach(function(element, index){
+     that.costSum += element.count*Number(element.price);
+    that.costSumString = that.costSum.toFixed(2);
+    console.log("Added: ", that.costSum);
+  });
             });
 
   }
@@ -32,5 +38,36 @@ console.log(this.cartItems);
   ionViewDidLoad() {
     console.log('ionViewDidLoad CartPage');
   }
+  
 
+  decreaseCount(product){
+      var that = this;
+    //  product.count--;
+      this.cartItems.forEach(function(element, index){
+         if(element.id == product.id){
+             element.count--;
+             console.log("Decremented the value in the cartItems array");
+             that.storage.set('cartProducts', that.cartItems);
+             console.log("Successfully made the changes in the storage element (Decremented)");
+         } 
+      }); 
+      console.log("Count decreased by 1 for: ", product.name);
+      this.costSum = this.costSum - Number(product.price);
+      this.costSumString = this.costSum.toFixed(2);
+  }
+  increaseCount(product){
+      var that = this;
+   //   product.count++;
+     this.cartItems.forEach(function(element, index){
+         if(element.id == product.id){
+             element.count++;
+             console.log("Incremented the value in the cartItems array");
+             that.storage.set('cartProducts', that.cartItems);
+             console.log("Successfully made the changes in the storage element (Incremented)");
+         } 
+      }); 
+      console.log("Count increased by 1 for: ", product.name);
+      this.costSum = this.costSum + Number(product.price);
+      this.costSumString = this.costSum.toFixed(2);
+  }
 }
