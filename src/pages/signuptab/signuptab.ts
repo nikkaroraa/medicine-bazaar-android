95fd510cd4aaceb3146bd2540cee453b64dc0054
dalip123 <dalipkumar703@gmail.com>
2017-01-10 22:40:52 +0530

    add login

--- src/pages/signuptab/signuptab.ts
+++ src/pages/signuptab/signuptab.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { FetchProducts } from '../../providers/fetch-products.service';
 /*
   Generated class for the Signuptab page.
 
@@ -9,13 +9,16 @@ import { NavController, NavParams } from 'ionic-angular';
 */
 @Component({
   selector: 'page-signuptab',
-  templateUrl: 'signuptab.html'
+  templateUrl: 'signuptab.html',
+  providers:[FetchProducts]
 })
 export class SignuptabPage {
   public newUser:any={};
   public billing_address:any={};
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  public user:any; 
+  constructor(public navCtrl: NavController, public navParams: NavParams,public fetchProducts:FetchProducts) {
+      this.newUser.isvalid=true;
+  }
   //user signup 
   signUp(newUser,billing_address)
   {
@@ -25,5 +28,67 @@ export class SignuptabPage {
  ionViewDidLoad() {
     console.log('ionViewDidLoad SignuptabPage');
   }
+ //check email
+ checkUserEmail(email)
+ {
+     
+    
+    var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if(!regex.test(email)){
+      
+      this.newUser.isValid = false;
+      /*
+      $ionicPopup.show({
+        template: "<center>Invalid Email! Please Check!</center>",
+        buttons: [{
+          text: 'OK'
+        }]
+      });
+      */
+        console.log(this.newUser.isValid);
+        console.log(email);
+        console.log("invalid email");
+      
+      return;
+      
+    }
+    
+    
+    this.fetchProducts.searchUser(email).subscribe(user => {
+        this.user=user;
+console.log(this.user);
+      });
+    /*
+    Woocommerce.get('customers/email/'+email,  function(err, data, res){
+      
+      if(err)
+        console.log(err);
+        
+      if(JSON.parse(this.user).customer){
+      
+       if(this.user) {
+        this.newUser.isValid = false;
+        
+        $ionicPopup.show({
+          template: "<center>EMail is already registered. Please login or use another email address.</center>",
+          buttons: [{
+            text: "Login",
+            onTap: function(e){
+              $state.go("app.login");
+            }
+          },{
+            text: "OK"
+          }]
+        })
+        
+      }
+      else{
+        this.newUser.isValid = true;
+      }
+  
+})
+*/
+ }
 
 }
