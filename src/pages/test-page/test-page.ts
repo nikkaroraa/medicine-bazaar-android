db31ba0213abded0d5656a57890fb41c72487aee
dalip123 <dalipkumar703@gmail.com>
2017-01-14 01:28:15 +0530

    push data to woocommerce

--- src/pages/test-page/test-page.ts
+++ src/pages/test-page/test-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {InAppBrowser} from 'ionic-native';
-import {Http, Headers, RequestOptions} from 'angular2/http';
+import { Http, Headers, RequestOptions} from 'angular2/http';
 import 'rxjs/Rx';
 import {Injectable} from 'angular2/core';
 import { Xyz } from '../../providers/xyz';
+import {FetchProducts } from '../../providers/fetch-products.service';
 
 
 
@@ -16,17 +17,30 @@ import { Xyz } from '../../providers/xyz';
 @Component({
   selector: 'page-test-page',
   templateUrl: 'test-page.html',
-  providers: [Xyz]
+  providers: [Xyz, FetchProducts]
 })
 
 export class TestPagePage {
   
-    
+    public data:any;
     
     
  
-  constructor(public xyz:Xyz) {
-     
+  constructor(public xyz:Xyz,public fetchProducts:FetchProducts) {
+    console.log("call create User ...");
+    this.fetchProducts.createUser().subscribe(data => {
+        // we've got back the raw data, now generate the core schedule data
+        // and save the data for later reference
+        this.data = data;
+        console.log(this.data);
+      },
+        err => {
+        console.log(err);
+    },
+        () => {
+        console.log('Completed');
+    });
+   
   }
 
   callPayment()
