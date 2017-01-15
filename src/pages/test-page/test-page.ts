970e7134909844709c8cb6bc783ddd92deeee748
dalip123 <dalipkumar703@gmail.com>
2017-01-15 09:38:21 +0530

    orders retreive

--- src/pages/test-page/test-page.ts
+++ src/pages/test-page/test-page.ts
@@ -5,7 +5,9 @@ import 'rxjs/Rx';
 import {Injectable} from 'angular2/core';
 import { Xyz } from '../../providers/xyz';
 import {FetchProducts } from '../../providers/fetch-products.service';
-
+import { SMS } from 'ionic-native';
+import { SendSms } from '../../providers/send-sms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 
 /*
@@ -17,20 +19,48 @@ import {FetchProducts } from '../../providers/fetch-products.service';
 @Component({
   selector: 'page-test-page',
   templateUrl: 'test-page.html',
-  providers: [Xyz, FetchProducts]
+  providers: [Xyz, FetchProducts,SendSms]
 })
 
 export class TestPagePage {
   
     public data:any;
-    
+    public customerOrder:any;
+    public verify:any={};
+    public verifyStatus:any;
     
  
-  constructor(public xyz:Xyz,public fetchProducts:FetchProducts) {
-    console.log("call create User ...");
-    this.fetchProducts.createUser().subscribe(data => {
-        // we've got back the raw data, now generate the core schedule data
-        // and save the data for later reference
+  constructor(public xyz:Xyz,public fetchProducts:FetchProducts,public sendSms:SendSms) {
+   // console.log("call get all orders ...");
+    
+     
+    // Orders for a particular customer
+    /*
+    this.fetchProducts.getAllOrders().subscribe(data => {
+        this.data = data;
+        for(var i=0;i<this.data.length;i++)
+        {
+          if(this.data[i].customer_id==2)
+          {
+            console.log(this.data[i].id);
+          }
+
+        }
+        console.log(this.data);
+      },
+        err => {
+        console.log(err);
+    },
+        () => {
+        console.log('Completed');
+    });
+   */
+  }
+  
+  //send sms to user
+  genSms()
+  {
+    this.sendSms.sendSMS().subscribe(data => {
         this.data = data;
         console.log(this.data);
       },
@@ -40,7 +70,23 @@ export class TestPagePage {
         () => {
         console.log('Completed');
     });
-   
+  }
+  //verify sms to user
+  verifyOTP(otp)
+  {
+    this.verify.countryCode="91";
+    this.verify.mobileNumber="9818418721";
+    this.verify.oneTimePassword=otp;
+    this.sendSms.verifySms(this.verify).subscribe(verifyStatus => {
+        this.verifyStatus = verifyStatus;
+        console.log(this.verifyStatus);
+      },
+        err => {
+        console.log(err);
+    },
+        () => {
+        console.log('Completed');
+    });
   }
 
   callPayment()
