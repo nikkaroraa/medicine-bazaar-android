6024166d6029ab3559b211f3545b6b0d2f037b1c
Nikhil Arora <narora200@gmail.com>
2017-01-16 10:00:22 +0530

    placeOrder completed

--- src/pages/test-page/test-page.ts
+++ src/pages/test-page/test-page.ts
@@ -9,7 +9,7 @@ import { SMS } from 'ionic-native';
 import { SendSms } from '../../providers/send-sms';
 import { FormBuilder, Validators } from '@angular/forms';
 import {AlertController} from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
 
 /*
   Generated class for the TestPage page.
@@ -30,9 +30,38 @@ export class TestPagePage {
     public customerOrder:any;
     public verify:any={};
     public verifyStatus:any;
-    
- 
-  constructor(public xyz:Xyz,public fetchProducts:FetchProducts,public sendSms:SendSms,private alertCtrl: AlertController) {
+    public newOrder = {};
+    public billing_address = {};
+    public productsArray: Array<any> = [];
+    public orderData = {};
+    public products: Array<any> = [];
+  constructor(public xyz:Xyz,public fetchProducts:FetchProducts,public sendSms:SendSms,private alertCtrl: AlertController, public storage:Storage) {
+
+
+    this.storage.get('cartProducts').then((val)=> {
+        
+       console.log('On the test-page: ', val);
+       
+
+       this.productsArray = val;
+         var that = this;
+       this.productsArray.forEach(function(element, index){
+          
+          that.products.push({product_id: element.id, quantity: element.count});
+        });
+
+        //this.cartArray = val;
+        
+
+/*this.cartItems = this.cartArray;
+console.log(this.cartItems);
+this.cartItems.forEach(function(element, index){
+     that.costSum += element.count*Number(element.price);
+    that.costSumString = that.costSum.toFixed(2);
+    console.log("Added: ", that.costSum);
+  });*/
+            });
+
    // console.log("call get all orders ...");
     
      
@@ -119,14 +148,56 @@ export class TestPagePage {
     });
   }
 */
-  callPayment()
-  {
-      
-  }
+  placeOrder(billing){
+    this.newOrder = {
+      "payment_method": "bacs",
+      "payment_method_title": "Direct Bank Transfer",
+      "set_paid": true,
+      "billing": {
+        "first_name": billing.first_name,
+        "last_name": billing.last_name,
+        "address_1": billing.address1,
+        "address_2": billing.address2,
+        "city": billing.city,
+        "state": billing.state,
+        "postcode": billing.postcode,
+        "country": billing.country,
+        "email": "dalipkumar703@gmail.com",
+        "phone": "9818418721"
+      },
+      "shipping": {
+                "first_name": billing.first_name,
+        "last_name": billing.last_name,
+        "address_1": billing.address1,
+        "address_2": billing.address2,
+        "city": billing.city,
+        "state": billing.state,
+        "postcode": billing.postcode,
+        "country": billing.country
+      },
+      "customer_id": 2,
+      "line_items": this.products
 
-  openBrowser(){
+    }
+    this.fetchProducts.placeOrder(this.newOrder).subscribe(data => {
+        // we've got back the raw data, now generate the core schedule data
+        // and save the data for later reference
+        this.orderData = data;
+        console.log(this.orderData);
+        //this.storage.set('customerID', this.customerData.id); //customerId set here
+        //this.createUserSuccessfull();
+      },
+        err => {
+        console.log(err);
+    },
+        () => {
+        console.log('Completed');
+    });
+
+  }
+/*  openBrowser(){
       let browser = new InAppBrowser('https://medicinebazaar.in/payumoney/index.php', '_blank')
       
   }
-
+*/
 }
