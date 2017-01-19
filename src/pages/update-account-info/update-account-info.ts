791ad45ef10956dfaa691a4cff0e3775ec4e9e2a
Nikhil Arora <narora200@gmail.com>
2017-01-19 20:11:19 +0530

    Update Password Working

--- src/pages/update-account-info/update-account-info.ts
+++ src/pages/update-account-info/update-account-info.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController,LoadingController  } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import firebase from 'firebase';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import {SigninModalPage} from '../signin-modal/signin-modal';
 
 /*
   Generated class for the UpdateAccountInfo page.
@@ -12,11 +17,70 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'update-account-info.html'
 })
 export class UpdateAccountInfoPage {
+  userProfile: any;
+  user : any;
+  updatePasswordForm: FormGroup;
+  submitAttempt: boolean = false;
+  loading: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public storage: Storage, public formBuilder: FormBuilder,
+  	public modalCtrl: ModalController, public loadingCtrl: LoadingController) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+ this.userProfile = firebase.database().ref('/userProfile');
+      console.log("UserProfile is: ", this.userProfile);
+  if(firebase.auth().currentUser){
+      this.user = firebase.auth().currentUser;
+      }
+      else{
+        this.user = {};
+      }
+      console.log("this.user ", this.user);
+      this.updatePasswordForm = formBuilder.group({
+        	password: ['',  Validators.compose([Validators.maxLength(30), Validators.required])]
+    	});
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UpdateAccountInfoPage');
   }
 
+	updatePassword(){
+		this.submitAttempt = true;
+    var that = this;
+		if(!this.updatePasswordForm.valid){
+         	console.log("Form not valid");
+    	} 
+    
+    	else 
+    	{
+	        console.log("Success! Form is valid. Continuing....")
+	        console.log(this.updatePasswordForm.value);
+        	this.user.updatePassword(this.updatePasswordForm.value.password).then(function() {
+  				  // Update successful.
+  				   that.loading = that.loadingCtrl.create({
+				      
+				    
+				      content: 'Password Updated Successfully!'
+				      
+				    });
+				    
+				    that.loading.present();
+
+				    setTimeout(() => {
+				      that.loading.dismiss();
+				     that.navCtrl.pop();
+				    }, 1000);
+  				  console.log("Password Updated Successfully");
+				  }, function(error) {
+  				  // An error happened.
+  				  console.log("Error in Password Updation. Please try again", error);
+  				  let signinModal = that.modalCtrl.create(SigninModalPage);
+  				  signinModal.present();
+  				  //recent sign in required
+				});
+
+    	}	
+	}
+
 }
+ 
