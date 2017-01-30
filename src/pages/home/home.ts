3cbab986398b8ea046215a746a6fe69b368fed3e
Nikhil Arora <narora200@gmail.com>
2017-01-30 12:36:50 +0530

    Taking into issue

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,15 +4,15 @@ import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';  
 import {MailSend} from '../../providers/mail-send';
 import {Camera} from 'ionic-native';
-import { Platform, ActionSheetController } from 'ionic-angular';
+import { Platform, ActionSheetController,ToastController } from 'ionic-angular';
 import { NavController} from 'ionic-angular';
 import {SearchPage} from '../search/search'; 
 import { Storage } from '@ionic/storage';   
 import {CartPage} from '../cart/cart';
 import { File,Transfer } from 'ionic-native';  
+import {AddressPage} from '../address/address';
 /*
   Generated class for the Home page.
-
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
@@ -29,8 +29,10 @@ export class HomePage {
     public base64Image: string;
 private imageSrc: string;
  public productCount: any = 0;
+ public CustomerEmail: any;
  mailResponse: any;
-  constructor(public mailSend:MailSend,public http: Http,public platform: Platform, public actionsheetCtrl: ActionSheetController, public navCtrl: NavController, public storage: Storage) {
+ public CustomerContact: any;
+  constructor(public toastCtrl: ToastController, public mailSend:MailSend,public http: Http,public platform: Platform, public actionsheetCtrl: ActionSheetController, public navCtrl: NavController, public storage: Storage) {
       
       this.storage.get('productCount').then((val)=>{
         if(!val){
@@ -53,7 +55,30 @@ navSearch(){
     this.navCtrl.push(this.searchPage);
 }
 openMenu() {
-    let actionSheet = this.actionsheetCtrl.create({
+       //check  if user exist
+     this.storage.get('customerEmail').then((value)=>{
+       console.log("Mobile:", value);
+        if(!value){
+            let toast = this.toastCtrl.create({
+        message: 'You need to login first OR Fill up the details!',
+        duration: 3000,
+        position: 'bottom'
+       });
+
+      toast.onDidDismiss(() => {
+        console.log('Dismissed toast');
+        this.navCtrl.setRoot(AddressPage); 
+      });
+
+      toast.present(toast);
+        }
+        else{
+          this.CustomerEmail=value;
+          this.storage.get('customerContact').then((val)=>{
+              if(val){
+
+                this.CustomerContact = val;
+                 let actionSheet = this.actionsheetCtrl.create({
       title: 'Choose an option to upload',
       cssClass: 'action-sheets-basic-page',
       buttons: [
@@ -70,7 +95,8 @@ openMenu() {
     }).then((imageData) => {
       // imageData is a base64 encoded string
         this.base64Image = "data:image/jpeg;base64," + imageData;
-        this.cameraImageSend(this.base64Image,imageData);
+        
+        this.cameraImageSend(this.base64Image,imageData,this.CustomerEmail, this.CustomerContact);
     }, (err) => {
         console.log(err);
     });
@@ -93,9 +119,10 @@ openMenu() {
 
   Camera.getPicture(cameraOptions)
    .then((file_uri) => {this.imageSrc = file_uri;
-           alert(this.imageSrc.split('/').pop());
-          alert("file location"+this.imageSrc);
-          this.fileTransfer(this.imageSrc);
+        //   alert(this.imageSrc.split('/').pop());
+        //  alert("file location"+this.imageSrc);
+        
+          this.fileTransfer(this.imageSrc,this.CustomerEmail, this.CustomerContact);
 
          
           /*
@@ -108,11 +135,17 @@ openMenu() {
         
       ]
     });
-    actionSheet.present();
+    actionSheet.present();   
+              }
+            });
+           
+        }
+      });
+    
 }
-fileTransfer(imageSrc)
+fileTransfer(imageSrc,CustomerEmail, CustomerContact)
  {
-   alert("file transfer working");
+   //alert("File transfer working");
    /*
  Cloudinary.v2.uploader.unsigned_upload(imageSrc, "cmoxms3e", 
     { cloud_name: "dtkd8f03m" }, 
@@ -122,15 +155,15 @@ fileTransfer(imageSrc)
    let filename = imageSrc.split('/').pop();
    let namePath = imageSrc.substr(0, imageSrc.indexOf('?'));
    let newFileName=filename.substr(0,filename.indexOf('?'));
-   alert("filename"+filename);
-   alert("namePath"+namePath);
-   alert("newFileName"+newFileName);
+  // alert("filename"+filename);
+  // alert("namePath"+namePath);
+  // alert("newFileName"+newFileName);
     let options = {
       fileKey: "file",
       fileName: newFileName,
       chunkedMode: false,
       mimeType: "image/jpg",
-      params : {'fileName': newFileName} ,
+      params : {'fileName': newFileName, 'email':CustomerEmail, 'contact':CustomerContact},  
       headers :{
           Connection: "close"
         }
@@ -141,12 +174,37 @@ fileTransfer(imageSrc)
  
     fileTransfer.upload(imageSrc, 'https://medicinebazaar.in/upload.php',
       options,true).then((entry) => {
-             alert(JSON.stringify(entry));        
-      }, (err) => {
-        alert(JSON.stringify(err));
+      //  alert(JSON.stringify(entry)); 
+        let toast = this.toastCtrl.create({
+        message: 'Upload Successfull!!! You will soon be contacted from our customer representative.',
+        duration: 3000,
+        position: 'bottom'
+       });
+
+      toast.onDidDismiss(() => {
+        console.log('Dismissed toast');
+         
       });
 
-  alert("finish working");
+      toast.present(toast);
+
+      }, (err) => {
+        let toast = this.toastCtrl.create({
+        message: 'An error was encountered while uploading, please try again! If the problem persists, please write out a review on our playstore space.',
+        duration: 3000,
+        position: 'bottom'
+       });
+
+      toast.onDidDismiss(() => {
+        console.log('Dismissed toast');
+         
+      });
+
+      toast.present(toast);
+      //  alert(JSON.stringify(err));
+      });
+
+//  alert("finish working");
 }
 
 
@@ -170,20 +228,46 @@ ionViewDidEnter(){
       });
 }
  //camera image send
-  cameraImageSend(base64,imageData)
+  cameraImageSend(base64,imageData,CustomerEmail, CustomerContact)
   {
      
-      this.mailSend.mailSending(base64,imageData).subscribe(mailResponse => {
+      this.mailSend.mailSending(base64,imageData,CustomerEmail, CustomerContact).subscribe(mailResponse => {
         this.mailResponse = mailResponse;
         console.log(this.mailResponse);
+          let toast = this.toastCtrl.create({
+        message: 'Upload Successfull!!! You will soon be contacted from our customer representative.',
+        duration: 3000,
+        position: 'bottom'
+       });
+
+      toast.onDidDismiss(() => {
+        console.log('Dismissed toast');
+         
+      });
+
+      toast.present(toast);
+
       },
         err => {
         console.log(err);
+         let toast = this.toastCtrl.create({
+        message: 'An error was encountered while uploading, please try again! If the problem persists, please write out a review on our playstore space.',
+        duration: 3000,
+        position: 'bottom'
+       });
+
+      toast.onDidDismiss(() => {
+        console.log('Dismissed toast');
+         
+      });
+
+      toast.present(toast);
     },
         () => {
         console.log('Completed');
     });
   }
+  /*
   //camera image send
   galleryImageSend(imageSrc,file_uri)
   {
@@ -199,8 +283,5 @@ ionViewDidEnter(){
         console.log('Completed');
     });
 }
-
+*/
 }
-    
-  
-
--- src/providers/mail-send.ts
+++ src/providers/mail-send.ts
@@ -14,22 +14,22 @@ export class MailSend {
   constructor(public http: Http) {
     console.log('Hello MailSend Provider');
   }
-  mailSending(base64,imageData)
+  mailSending(base64,imageData,CustomerEmail,CustomerContact)
   {
-  	 let requestHeaders = new Headers({ 
-  	 "Content-Type": "application/json"
-  	 });
+     let requestHeaders = new Headers({ 
+     "Content-Type": "application/json"
+     });
      this.data={
     "key": "raKKjF2xJOgZb17DN8XNPg",
     "message": {
-        "html": "<p>Example HTML content</p>",
-        "text": "Example text content",
-        "subject": "example subject",
+        "html": "<p> Customer E-mail ID: "+ CustomerEmail + "</p><p> Customer Contact Information " + CustomerContact+"</p>",
+        "text": "Testing123",
+        "subject": "New User Prescription",
         "from_email": "admin@medicinebazaar.in",
         "from_name": "Example Name",
         "to": [
             {
-                "email":"dalipkumar703@gmail.com",
+                "email":"genithub@gmail.com",
                 "name": "Recipient Name",
                 "type": "to"
             }
